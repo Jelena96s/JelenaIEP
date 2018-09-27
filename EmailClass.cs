2b735984c074da1f845cae5ba3243f5e062dc9f2
Jelena96s <38372925+Jelena96s@users.noreply.github.com>
2018-09-27 14:28:19 +0200

    Add files via upload

---
+++ EmailClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Projekat
+{
+    public static class EmailClass
+    {
+        public static void Email(string mailTo, string subject, string body)
+        {
+            MailMessage message = new MailMessage();
+            MailAddress addressTo = new MailAddress(mailTo);
+            MailAddress addressFrom = new MailAddress("jelena96sm@gmail.com");
+            message.To.Add(addressTo);
+            message.From = addressFrom;
+            message.Subject = subject;
+            message.Body = body;
+           
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "jelena96sm@gmail.com",
+                    Password = "mimijeja"
+                };
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = credential;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+            }
+           
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.SignalR.Configuration;
+using Microsoft.Owin;
+using Owin;
+using Projekat.Models;
+using System.Data.Entity;
+using System.Linq;
+
+[assembly: OwinStartupAttribute(typeof(Projekat.Startup))]
+namespace Projekat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            DefaultParameters();
+        }
+
+        public void DefaultParameters()
+        {
+            AuctionDb db = new AuctionDb();
+            AdminSettings admin = db.adminSettings.FirstOrDefault();
+
+            admin.N = 10;
+
+            admin.D = 18000;
+
+            admin.S = 30;
+
+            admin.G = 50;
+
+            admin.P = 100;
+
+            admin.C = "RSD";
+
+            admin.T = 100;
+
+            db.Entry(admin).State = EntityState.Modified;
+            db.SaveChanges();
+        
+    }
+
+    }
+}
